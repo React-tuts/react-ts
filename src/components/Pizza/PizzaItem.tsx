5661934bc97000fb617b85b4e7181731c110c4b7
raja <rashok-adm@transportforlondon.onmicrosoft.com>
2026-04-05 21:57:08 +0100

    changes

--- src/components/Pizza/PizzaItem.tsx
+++ src/components/Pizza/PizzaItem.tsx
@@ -7,7 +7,7 @@ import { PizzaItemProps } from "./Pizza.types";
 export const PizzaItem = ({ pizza }: PizzaItemProps) => {
   //if (pizza.soldOut) return null;
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizza.soldOut ? "sold-out" : null}`}>
       <img src={`/images/${pizza.photoName}`} alt={`${pizza.name} pizza`} />
       <div>
         <h3>{pizza.name}</h3>

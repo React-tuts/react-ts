5fbeaf0bae0fa943b9a31787281d946e1c49356e
raja <rashok-adm@transportforlondon.onmicrosoft.com>
2026-04-05 21:51:24 +0100

    changes

--- src/components/Pizza/PizzaItem.tsx
+++ src/components/Pizza/PizzaItem.tsx
@@ -1,15 +1,19 @@
-import { PizzaItemProps } from "./Pizza.types"
+import { PizzaItemProps } from "./Pizza.types";
 /**
- * 
+ *
  * @param PizzaItemProps
  * @returns JSX
  */
-export const PizzaItem = ({pizza}: PizzaItemProps) => {
-    if (pizza.soldOut) return null;
-    return <li className="pizza">
-            <img src = {`/images/${pizza.photoName}`} alt={`${pizza.name} pizza`}  />
-            <h3>{pizza.name}</h3>
-            <p>{pizza.ingredients}</p>
-            <span>{pizza.price}</span>
-         </li>
-}
+export const PizzaItem = ({ pizza }: PizzaItemProps) => {
+  //if (pizza.soldOut) return null;
+  return (
+    <li className="pizza">
+      <img src={`/images/${pizza.photoName}`} alt={`${pizza.name} pizza`} />
+      <div>
+        <h3>{pizza.name}</h3>
+        <p>{pizza.ingredients}</p>
+        <span>{pizza.soldOut ? `SOLD OUT` : pizza.price}</span>
+      </div>
+    </li>
+  );
+};
